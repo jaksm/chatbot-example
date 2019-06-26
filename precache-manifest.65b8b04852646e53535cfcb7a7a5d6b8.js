4606a022e771cdaf5a553821b70d7e9f17917f7c
Jaksa Malisic <jaksa.malisic@gmail.com>
2019-06-26 16:30:33 +0200

    Updates

---
+++ precache-manifest.65b8b04852646e53535cfcb7a7a5d6b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7d1be640ca9e2d2cc4b1a121beaec6",
+    "url": "/chatbot-example/index.html"
+  },
+  {
+    "revision": "4a2a21a512fb52b13449",
+    "url": "/chatbot-example/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "18dc0cbf98d681ced1be",
+    "url": "/chatbot-example/static/js/2.48e71a8a.chunk.js"
+  },
+  {
+    "revision": "4a2a21a512fb52b13449",
+    "url": "/chatbot-example/static/js/main.b4369c6a.chunk.js"
+  },
+  {
+    "revision": "d522da2643b02114ab54",
+    "url": "/chatbot-example/static/js/runtime~main.57a86b7b.js"
+  },
+  {
+    "revision": "8eb88d518375fe830a64b1c197d266ee",
+    "url": "/chatbot-example/static/media/avatar.8eb88d51.jpg"
+  }
+]);
